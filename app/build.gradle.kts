49feb70efadf82089041863da51ff4d015ae7b53
guillemulass <gmulbat1301@g.educaand.es>
2024-06-04 16:28:46 +0200

    Trabajando en subida de imagenes a firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -67,7 +66,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -76,31 +74,27 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    //
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("com.google.firebase:firebase-firestore-ktx:24.10.1")
 
+    // Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
-    implementation("com.google.firebase:firebase-analytics")
 
-    
-    // Firebase Realtime Database
-    implementation("com.google.firebase:firebase-database-ktx:20.0.5")
-    // Firebase Authentication (if needed)
-    implementation("com.google.firebase:firebase-auth-ktx:21.0.3")
-
-
+    // Firebase dependencies
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
 
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
 
-    // Implementaciones de RetroFit
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("io.coil-kt:coil-compose:2.5.0")
 
-    // Implementacion permisos
+    // Accompanist permissions
     implementation("com.google.accompanist:accompanist-permissions:0.29.1-alpha")
-
 }
