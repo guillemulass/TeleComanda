e33705bcdf6ccd6d1df4caefed88d53ffef409c9
guillemulass <gmulbat1301@g.educaand.es>
2024-06-11 20:06:41 +0200

    1st commit Figma

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-android")
-    id("com.google.relay") version "0.3.02"
+    id("com.google.relay") version "0.3.12"
     id("com.google.gms.google-services")
 }
 
--- app/src/main/java/com/example/telecomanda/screens/initialScreens/InitialScreen.kt
+++ app/src/main/java/com/example/telecomanda/screens/initialScreens/InitialScreen.kt
@@ -1,33 +1,32 @@
 package com.example.telecomanda.screens.initialScreens
 
-import android.Manifest
-import android.os.Build
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.telecomanda.botonbig32sp.BotonBig32sp
 import com.example.telecomanda.routes.Routes
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberPermissionState
 
 @Composable
 fun InitialScreen(
     navController: NavHostController,
 ) {
 
-
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF161618))
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -47,25 +46,21 @@ fun InitialScreen(
 
             Spacer(modifier = Modifier.height(32.dp))
 
-            Button(
+            BotonBig32sp(
                 onClick = {
                     navController.navigate(Routes.WorkerSelectionScreenRoute.route)
                 },
-                modifier = Modifier
-            ) {
-                Text(text = "Trabajador")
-            }
+                text = "Trabajador"
+            )
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(
+            BotonBig32sp(
                 onClick = {
                     navController.navigate(Routes.TableCodeInputScreenRoute.route)
                 },
-                modifier = Modifier
-            ) {
-                Text(text = "Cliente")
-            }
+                text = "Cliente"
+            )
         }
     }
 }
